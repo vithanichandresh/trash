ec6d80b82e3d40e397d6d1a877c9a657ecd899b3
vithanichandresh <vithanichandresh.g@gmail.com>
2025-07-18 16:32:36 +0530

    feat: Integrate local notifications and device ID retrieval
    This commit introduces the following changes:
    
    - **Android Configuration:**
        - Updated `ndkVersion` to "27.0.12077973".
        - Enabled `coreLibraryDesugaring`.
        - Increased `minSdk` to 26.
        - Updated `desugar_jdk_libs` dependency to version "2.1.5".
    - **Dependencies:**
        - Added `flutter_local_notifications: ^19.3.1` for displaying local notifications.
        - Added `platform_device_id_plus: ^1.0.7` for retrieving unique device identifiers.
        - Updated `webview_flutter_android` to version `^4.8.2`.
    - **File Deletion:**
        - Removed `android/.kotlin/sessions/kotlin-compiler-12493983409031612218.salive`.
    - **Build Report:**
        - Updated the Android build problems report.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,12 @@ plugins {
 android {
     namespace = "com.trash.flutter_demo"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -24,7 +25,7 @@ android {
         applicationId = "com.trash.flutter_demo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -41,7 +42,7 @@ android {
     }
 }
 dependencies {
-    implementation("com.android.tools:desugar_jdk_libs:2.0.3") // Add desugaring dependency
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5") // Add desugaring dependency
 }
 flutter {
     source = "../.."
